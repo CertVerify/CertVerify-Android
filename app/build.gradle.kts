d07c0b24155dfd713257af436db78f74dce764cb
Kanahia <kaushalkanahia@gmail.com>
2023-12-16 23:49:24 +0530

    Setup SignIn and SignUp page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.kanahia.certverify"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.kanahia.certverify"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/kanahia/certverify/MainActivity.kt
+++ app/src/main/java/com/kanahia/certverify/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.kanahia.certverify
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val timer = object : CountDownTimer(2000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                // This method will be called every second until the timer is finished
+                // You can update UI or perform any action here if needed
+            }
+
+            override fun onFinish() {
+                // This method will be called when the timer is finished (after 2 seconds in this case)
+                // You can navigate to the main activity or perform any other action here
+                startMainActivity()
+            }
+        }
+
+        // Start the timer when the splash screen activity is created
+        timer.start()
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(this, SignIn::class.java)
+        startActivity(intent)
+        finish() // Optional: finish the splash screen activity so that it's not in the back stack
     }
 }
