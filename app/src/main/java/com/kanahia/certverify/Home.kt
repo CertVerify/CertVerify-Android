f9d3ed53fcc40f5ae8dc55e7bb5c53446674cb5d
Kanahia <kaushalkanahia@gmail.com>
2023-12-17 15:09:05 +0530

    Created profile activity

--- app/src/main/java/com/kanahia/certverify/Home.kt
+++ app/src/main/java/com/kanahia/certverify/Home.kt
@@ -1,11 +1,20 @@
 package com.kanahia.certverify
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class Home : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        var profileCrd = findViewById<CardView>(R.id.profileCardView)
+        profileCrd.setOnClickListener {
+            startActivity(Intent(this,Profile::class.java))
+        }
     }
 }
